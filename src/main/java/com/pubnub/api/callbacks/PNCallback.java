ce37b085fd154e34555dac25e02ad69a535cd26d
QSD <info@qsdbih.com>
2019-08-29 09:59:17 +0200

    Set up data for GH Pages

--- src/main/java/com/pubnub/api/callbacks/PNCallback.java
+++
@@ -1,9 +0,0 @@
-package com.pubnub.api.callbacks;
-
-
-import com.pubnub.api.models.consumer.PNStatus;
-
-public abstract class PNCallback<X> {
-    public abstract void onResponse(X result, PNStatus status);
-}
-
