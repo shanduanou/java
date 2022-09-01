80173e7fa17c5b1c176c6f558c66c9714f1414e2
marcin.cebo <marcin.cebo@pubnub.com>
2022-09-01 12:20:53 +0200

    Added upsertUser operation to replace setUUIDMetadata
    Under the hood ObjectV3 endpoint is used.
    This is initial shape, and we are expecting Payloads to change in the future.

--- src/main/java/com/pubnub/api/models/server/objects_vsp/CreateUserPayload.java
+++ src/main/java/com/pubnub/api/models/server/objects_vsp/CreateUserPayload.java
@@ -1,10 +1,5 @@
 package com.pubnub.api.models.server.objects_vsp;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
-
-
 public class CreateUserPayload extends UserPayload {
     public CreateUserPayload(String name, String email, String externalId, String profileUrl, Object custom, String status, String type) {
         super(name, email, externalId, profileUrl, custom, status, type);
--- src/main/java/com/pubnub/api/models/server/objects_vsp/UpsertUserPayload.java
+++ src/main/java/com/pubnub/api/models/server/objects_vsp/UpsertUserPayload.java
@@ -1,6 +1,6 @@
 package com.pubnub.api.models.server.objects_vsp;
 
-public class UpsertUserPayload extends UserPayload{
+public class UpsertUserPayload extends UserPayload {
     public UpsertUserPayload(String name, String email, String externalId, String profileUrl, Object custom, String status, String type) {
         super(name, email, externalId, profileUrl, custom, status, type);
     }
